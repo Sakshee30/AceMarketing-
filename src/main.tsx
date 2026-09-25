51252fc6a3d1c511e596bbd6be8a2b7ea698bce9
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-25 16:46:26 +0530

    Initialize AceMarketing tracking on application load

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AcePlatform from './AcePlatform'
+import { installAceTracking } from './lib/tracker'
+
+installAceTracking()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
