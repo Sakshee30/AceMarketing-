a585dcad70d0903ee11f23f590c7f89bf0d9c812
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-26 00:24:55 +0530

    feat: mount dashboard section navigator

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AcePlatform from './AcePlatform'
+import DashboardQuickNav from './DashboardQuickNav'
+import './dashboard-quick-nav.css'
 import { installAceTracking } from './lib/tracker'
 
 installAceTracking()
@@ -8,5 +10,6 @@ installAceTracking()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AcePlatform />
+    <DashboardQuickNav />
   </React.StrictMode>
 )
